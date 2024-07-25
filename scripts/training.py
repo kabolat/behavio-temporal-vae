99344b897b5858ff95b136f4e8ecb48a7a104dfb
kabolat <k.bolat@tudelft.nl>
2024-07-25 17:32:51 +0200

    Moved data preperation out.

--- scripts/training.py
+++ scripts/training.py
@@ -20,78 +20,6 @@ from src.user_encoding_lib import UserEncoder
 def load_config(json_file):
     with open(json_file, 'r') as file: return json.load(file)
 
-def prepare_data(config_data):
-    RANDOM_SEED = config_data["random_seed"]
-    DATASET_DIR = config_data["dataset_dir"]
-    DATASET_NAME = config_data["dataset_name"]
-    CONDITION_TAG_LIST = config_data["condition_tag_list"]
-    RESOLUTION = config_data["resolution"] #in hours
-    PAD = config_data["pad"] #in features
-    AMPUTE_PARAMS = config_data["ampute_params"]
-    USER_SUBSAMPLE_RATE, DAY_SUBSAMPLE_RATE = config_data["subsample_rate"]["user"], config_data["subsample_rate"]["day"]
-    SHIFT = config_data["scaling"]["shift"]
-    ZERO_ID = config_data["scaling"]["zero_id"]
-    LOG_SPACE = config_data["scaling"]["log_space"]
-    VAL_RATIO = config_data["val_ratio"]
-    USER_EMBEDDING_KWARGS = config_data["user_embedding_kwargs"]
-    NUM_TOPICS = USER_EMBEDDING_KWARGS["model_kwargs"]["num_topics"]
-    VOCAB_SIZE = USER_EMBEDDING_KWARGS["model_kwargs"]["num_clusters"]
-    USER_EMBEDDING_KWARGS["model_kwargs"]["random_state"] = RANDOM_SEED
-    USER_EMBEDDING_KWARGS["model_kwargs"]["user_subsample_rate"]: USER_SUBSAMPLE_RATE
-    USER_EMBEDDING_KWARGS["fit_kwargs"]["lda"]["doc_topic_prior"] = 1.0/NUM_TOPICS
-    USER_EMBEDDING_KWARGS["fit_kwargs"]["lda"]["topic_word_prior"] = 1.0/VOCAB_SIZE
-
-    ## Import data
-    dataset_path = os.path.join(DATASET_DIR, DATASET_NAME)
-    df = pd.read_csv(os.path.join(dataset_path, 'dataset.csv'))
-    data, dates = df.iloc[:,:-2].values, df.date.values
-    num_days, num_users = df.date.nunique(), df.user.nunique()
-    print(f'Dataset: {DATASET_NAME}')
-    print(f'Loaded {len(data)} consumption profiles from {num_days} dates and {num_users} users.')
-
-    date_dict = np.load(os.path.join(dataset_path, 'encode_dict.npy'), allow_pickle=True).item()["date_dict"]
-    date_dict_inv = {v: k for k, v in date_dict.items()}
-    if not os.path.exists(os.path.join(dataset_path, 'raw_dates.npy')):
-        raw_dates = np.array([datetime.datetime.strptime(date_dict_inv[d], '%Y-%m-%d') for d in dates])
-        np.save(os.path.join(dataset_path, 'raw_dates.npy'), raw_dates)
-    else: raw_dates = np.load(os.path.join(dataset_path, 'raw_dates.npy'), allow_pickle=True)
-    metadata = pd.read_csv(os.path.join(dataset_path, 'metadata.csv'))
-    unique_provinces = metadata.province.unique()
-    print(f'Loaded metadata for {len(unique_provinces)} provinces')
-    print(f"Uniqe provinces are: {unique_provinces}")
-
-    ## Preprocess data
-
-    ## Set resolution and pad
-    X = preprocess_lib.downsample_and_pad(np.reshape(data, (num_users, num_days, -1)), RESOLUTION, PAD)
-    X, user_mask = preprocess_lib.remove_unwanted_profiles(X)
-    X, raw_dates = preprocess_lib.subsample_data(X, np.reshape(raw_dates, (num_users, num_days))[user_mask], USER_SUBSAMPLE_RATE, DAY_SUBSAMPLE_RATE)
-
-    num_users, num_days, num_features = X.shape
-
-    print("{:.<40}{:.>5}".format("Number of (subsampled/filtered) users", num_users))
-    print("{:.<40}{:.>5}".format("Number of (subsampled) days", num_days))
-    print("{:.<40}{:.>5}".format("Number of (aggregated) features", num_features))
-
-    X_missing, missing_idx, num_missing_profiles, missing_days = preprocess_lib.ampute_data(X, a=AMPUTE_PARAMS["a"], b=AMPUTE_PARAMS["b"], random_seed=RANDOM_SEED)
-    
-    nonzero_mean, nonzero_std = utils.zero_preserved_log_stats(X_missing)
-    X_missing = utils.zero_preserved_log_normalize(X_missing, nonzero_mean, nonzero_std, log_output=LOG_SPACE, zero_id=ZERO_ID, shift=SHIFT)
-
-    condition_kwargs, condition_set = conditioning_lib.prepare_conditions(CONDITION_TAG_LIST, raw_dates, data=X_missing.reshape(num_users, num_days, -1), dataset_path=dataset_path, user_embedding_kwargs=USER_EMBEDDING_KWARGS, config_dict=config_data)
-
-    X_observed, user_ids_observed, condition_set_observed, X_test_flat, X_test_list, user_ids_test, condition_set_test = preprocess_lib.separate_test_set(X, X_missing, condition_set, missing_idx, missing_days)
-
-    X_train, user_ids_train, conditions_train, X_val, user_ids_val, conditions_val = preprocess_lib.separate_val_set(X_observed, user_ids_observed, condition_set_observed, val_ratio=VAL_RATIO, random_seed=RANDOM_SEED)
-
-    conditioner = conditioning_lib.Conditioner(**condition_kwargs, condition_set=condition_set_observed)
-    trainset = datasets.ConditionedDataset(inputs=X_train, conditions=conditions_train, conditioner=conditioner)
-    valset = datasets.ConditionedDataset(inputs=X_val, conditions=conditions_val, conditioner=conditioner)
-    print(f"Number of Training Points: {len(trainset)}")
-    print(f"Number of Validation Points: {len(valset)}")
-
-    return trainset, valset, conditioner
-
 def train_model(model, trainset, valset, train_kwargs, writer=None):
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=train_kwargs["batch_size"], shuffle=True, drop_last=True, num_workers=1, pin_memory=True)
     valloader = torch.utils.data.DataLoader(valset, batch_size=8196, shuffle=False, drop_last=False, num_workers=1, pin_memory=True)
@@ -104,11 +32,10 @@ def train_model(model, trainset, valset, train_kwargs, writer=None):
     model.eval()
     return model
 
-
 def main(args):
     config = load_config(args.config_path)
 
-    trainset, valset, conditioner = prepare_data(config["data"])
+    trainset, valset, conditioner, _, _, _ = preprocess_lib.prepare_data(config["data"])
 
     model = CVAE(input_dim=trainset.inputs.shape[-1], conditioner=conditioner, **config["model"])
     print("Number of encoder parameters:", model.encoder._num_parameters())
--- src/preprocess_lib.py
+++ src/preprocess_lib.py
@@ -1,4 +1,9 @@
 import numpy as np
+import pandas as pd
+import os
+import datetime
+
+from . import utils, conditioning_lib, datasets
 
 def downsample_and_pad(data, resolution=1, pad=0):
     num_features = data.shape[-1]
@@ -78,3 +83,60 @@ def separate_val_set(data, user_ids, condition_set, val_ratio=0.1, random_seed=N
     conditions_train, conditions_val = {k: v[train_idx] for k, v in condition_set.items()}, {k: v[val_idx] for k, v in condition_set.items()}
 
     return X_train, user_ids_train, conditions_train, X_val, user_ids_val, conditions_val
+
+def prepare_data(config_data):
+    config_data["user_embedding_kwargs"]["fit_kwargs"]["lda"]["doc_topic_prior"] = 1.0/config_data["user_embedding_kwargs"]["model_kwargs"]["num_topics"]
+    config_data["user_embedding_kwargs"]["fit_kwargs"]["lda"]["topic_word_prior"] = 1.0/config_data["user_embedding_kwargs"]["model_kwargs"]["num_clusters"]
+
+    ## Import data
+    dataset_path = os.path.join(config_data["dataset_dir"], config_data["dataset_name"])
+    df = pd.read_csv(os.path.join(dataset_path, 'dataset.csv'))
+    data, dates = df.iloc[:,:-2].values, df.date.values
+    num_days, num_users = df.date.nunique(), df.user.nunique()
+    print(f'Dataset: {config_data["dataset_name"]}')
+    print(f'Loaded {len(data)} consumption profiles from {num_days} dates and {num_users} users.')
+
+    date_dict = np.load(os.path.join(dataset_path, 'encode_dict.npy'), allow_pickle=True).item()["date_dict"]
+    date_dict_inv = {v: k for k, v in date_dict.items()}
+    if not os.path.exists(os.path.join(dataset_path, 'raw_dates.npy')):
+        raw_dates = np.array([datetime.datetime.strptime(date_dict_inv[d], '%Y-%m-%d') for d in dates])
+        np.save(os.path.join(dataset_path, 'raw_dates.npy'), raw_dates)
+    else: raw_dates = np.load(os.path.join(dataset_path, 'raw_dates.npy'), allow_pickle=True)
+    metadata = pd.read_csv(os.path.join(dataset_path, 'metadata.csv'))
+    unique_provinces = metadata.province.unique()
+    print(f'Loaded metadata for {len(unique_provinces)} provinces')
+    print(f"Uniqe provinces are: {unique_provinces}")
+
+    ## Preprocess data
+    X = downsample_and_pad(np.reshape(data, (num_users, num_days, -1)), config_data["resolution"], config_data["pad"])
+    X, user_mask = remove_unwanted_profiles(X)
+    X, raw_dates = subsample_data(X, np.reshape(raw_dates, (num_users, num_days))[user_mask], config_data["subsample_rate"]["user"], config_data["subsample_rate"]["day"])
+
+    num_users, num_days, num_features = X.shape
+
+    print("{:.<40}{:.>5}".format("Number of (subsampled/filtered) users", num_users))
+    print("{:.<40}{:.>5}".format("Number of (subsampled) days", num_days))
+    print("{:.<40}{:.>5}".format("Number of (aggregated) features", num_features))
+
+    X_missing, missing_idx, num_missing_profiles, missing_days = ampute_data(X, a=config_data["ampute_params"]["a"], b=config_data["ampute_params"]["b"], random_seed=config_data["random_seed"])
+    
+    nonzero_mean, nonzero_std = utils.zero_preserved_log_stats(X_missing)
+    X_missing = utils.zero_preserved_log_normalize(X_missing, nonzero_mean, nonzero_std, log_output=config_data["scaling"]["log_space"], zero_id=config_data["scaling"]["zero_id"], shift=config_data["scaling"]["shift"])
+
+    condition_kwargs, condition_set = conditioning_lib.prepare_conditions(config_data["condition_tag_list"], raw_dates, data=X_missing.reshape(num_users, num_days, -1), dataset_path=dataset_path, user_embedding_kwargs=config_data["user_embedding_kwargs"], config_dict=config_data)
+
+    X_observed, user_ids_observed, condition_set_observed, X_test_flat, X_test_list, user_ids_test, condition_set_test = separate_test_set(X, X_missing, condition_set, missing_idx, missing_days)
+
+    X_train, user_ids_train, conditions_train, X_val, user_ids_val, conditions_val = separate_val_set(X_observed, user_ids_observed, condition_set_observed, val_ratio=config_data["val_ratio"], random_seed=config_data["random_seed"])
+
+    conditioner = conditioning_lib.Conditioner(**condition_kwargs, condition_set=condition_set_observed)
+    trainset = datasets.ConditionedDataset(inputs=X_train, conditions=conditions_train, conditioner=conditioner)
+    valset = datasets.ConditionedDataset(inputs=X_val, conditions=conditions_val, conditioner=conditioner)
+    print(f"Number of Training Points: {len(trainset)}")
+    print(f"Number of Validation Points: {len(valset)}")
+
+    user_ids = {"train": user_ids_train, "val": user_ids_val, "test": user_ids_test}
+    condition_set = {"train": conditions_train, "val": conditions_val, "test": condition_set_test}
+    X_test = {"flat": X_test_flat, "list": X_test_list}
+
+    return trainset, valset, conditioner, user_ids, condition_set, X_test
