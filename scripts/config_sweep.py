23a86e9ace1b94a09cb3620e054a6bd1cc09d543
kabolat <k.bolat@tudelft.nl>
2024-09-01 17:27:04 +0200

    Check validity for asymmetric configs

--- scripts/config_sweep.py
+++ scripts/config_sweep.py
@@ -34,30 +34,20 @@ def get_hyperparameters_combinations(hyperparameters):
     expanded_hyperparameters = expand_dict(hyperparameters)
     return generate_combinations(expanded_hyperparameters)
 
-def check_validity(combination, config):
-    keys = []
-    for key_path in combination.keys():
-        keys.append(key_path.split('.'))
+def check_validity(config):
+    # Add checks here
+    if config["model"]["distribution_dict"]["likelihood"]["num_neurons"] != config["model"]["distribution_dict"]["posterior"]["num_neurons"]: return False
+    if config["model"]["distribution_dict"]["likelihood"]["num_hidden_layers"] != config["model"]["distribution_dict"]["posterior"]["num_hidden_layers"]: return False
+    if config["model"]["distribution_dict"]["likelihood"]["dropout"] != config["model"]["distribution_dict"]["posterior"]["dropout"]: return False
 
-    # if "users" not in config["data"]["condition_tag_list"]:
-    #     for key in keys:
-    #         if "user_embedding_kwargs" in key: return False
-    
-    # if config["model"]["distribution_dict"]["likelihood"]["dist_type"] != "dict-gauss":
-    #     for key in keys:
-    #         if "vocab_size" in key: return False
     return True
 
 def apply_rules(config, hyperparameters):
     hyperparameter_combinations = get_hyperparameters_combinations(hyperparameters)
-    # mask = [True] * len(hyperparameter_combinations)
     # for i, combination in enumerate(hyperparameter_combinations):
     #     updated_config = copy.deepcopy(config)
     #     for key, value in combination.items(): update_config(updated_config, key, value)
-    #     mask[i] = check_validity(combination, updated_config) # Check if the combination is valid
-    # shifted_mask = [True] + mask[:-1]
-    # memory_mask = [mask[i]|shifted_mask[i] for i in range(len(mask))]
-    # hyperparameter_combinations = [hyperparameter_combinations[i] for i in range(len(mask)) if memory_mask[i]] # Too keep the first invalid combination for ignoring
+
     return hyperparameter_combinations
 
 def run_sweep(config, hyperparameters):
@@ -65,6 +55,10 @@ def run_sweep(config, hyperparameters):
     for i, combination in enumerate(hyperparameter_combinations):
         updated_config = copy.deepcopy(config)
         for key, value in combination.items(): update_config(updated_config, key, value)
+        if not check_validity(updated_config):
+            print(f"INVALID COMBINATION!")
+            print(f"Skipping combination {i + 1}/{len(hyperparameter_combinations)}: {combination}")
+            continue
         print(f"Running combination {i + 1}/{len(hyperparameter_combinations)}: {combination}")
         main(updated_config)  # Assuming main() accepts a config dictionary
 
@@ -72,32 +66,43 @@ if __name__ == "__main__":
     config_path = './config_files/config0.json'
     base_config = load_config(config_path)
 
-    base_config["save_dir"] = "runs/sweep_runs"
+    base_config["save_dir"] = "runs/sweep_runs_corrected"
     base_config["save_tag"] = "sweep_"
 
     hyperparameters = {
         "data": {
-            "pad": [0],
+            # "pad": [0],
             "ampute_params": {
-                "a": [0.85],
+                # "a": [0.85],
                 "b": [5, 10, 30]
             },
             "condition_tag_list": 
-                [["months", "weekdays", "users"]],
+                [["months", "weekdays"], ["months", "weekdays", "users"]],
             "user_embedding_kwargs": {
                 "model_kwargs": {
-                    "num_topics": [5, 20, 50],
-                    "num_clusters": [100, 500, 1000]
+                    "num_topics": [100],
+                    "num_clusters": [1000]
                 }
             }
         },
         "model": {
             "distribution_dict": {
+                "posterior": {
+                    # "num_neurons": [1000],
+                    # "num_hidden_layers": [2],
+                    "dropout": [False]
+                },
                 "likelihood": {
                     "dist_type": ["dict-gauss"],
-                    "vocab_size": [100]
+                    # "num_neurons": [1000],
+                    # "num_hidden_layers": [2],
+                    "vocab_size": [25, 50, 250, 500],
+                    "dropout": [False]
                 }
             }
+        },
+        "train": {
+            "beta": [1.0],
         }
     }
 
