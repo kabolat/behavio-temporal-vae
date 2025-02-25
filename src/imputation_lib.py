fabe5b32d36d884c6b801e0e518cf1f0e606d302
kabolat <k.bolat@tudelft.nl>
2025-02-25 16:23:34 +0100

    batch imputo-forecast adaptation

--- src/imputation_lib.py
+++ src/imputation_lib.py
@@ -25,9 +25,9 @@ def sample_conditional(model, param_dict, x_observed, observed_indices, num_samp
 
     conditional_mean, conditional_cov = get_conditonal_params(model, param_dict, x_observed, observed_indices)
 
-    eps = torch.randn((num_samples, model.output_dim - len(observed_indices)), device=conditional_mean.device)
+    eps = torch.randn((num_samples, x_observed.shape[0], model.output_dim - len(observed_indices)), device=conditional_mean.device)
     L_cond = torch.linalg.cholesky(conditional_cov)
-    cond_samples = conditional_mean + eps @ L_cond.mT  
+    cond_samples = conditional_mean + (L_cond @ eps.unsqueeze(-1)).squeeze(-1)
 
     return cond_samples, conditional_mean, conditional_cov
 
@@ -36,18 +36,18 @@ def pseudo_gibbs(model, x, missing_mask, conditioner, condition_set, num_samples
 
     x_samples, z_samples, conditions_list = list(), list(), list()
     
-    observed_indices = torch.where((~missing_mask)[0])[0]
+    observed_indices = torch.where((~missing_mask)[0])[0] ## WARNING: It works only if the missing parts in the batch are all the same!
 
     for itx in range(num_iter):
         conditions = torch.tensor(conditioner.transform({k: v for k, v in condition_set.items()})).float().to(device)
         x_rec, z_rec = model.forward(x, conditions)
 
         cond_sample, _, _ = sample_conditional(model.decoder, {k:v[0] for k,v in x_rec["params"].items()}, x[:,observed_indices], observed_indices)
-        x[missing_mask] = cond_sample
+        x[missing_mask] = cond_sample.flatten()
 
-        x_samples.append(x[0].cpu().tolist())
-        z_samples.append(z_rec["samples"][0,0].cpu().tolist())
-        conditions_list.append(conditions[0].cpu().tolist())
+        x_samples.append(x.cpu().tolist())
+        z_samples.append(z_rec["samples"][0].cpu().tolist())
+        conditions_list.append(conditions.cpu().tolist())
 
         if itx>=num_samples: 
             x_samples.pop(0)
@@ -61,12 +61,12 @@ def metropolis_within_gibbs(model, x, missing_mask, conditioner, condition_set, 
     
     x_samples, z_samples, conditions_list = list(), list(), list()
 
-    observed_indices = torch.where((~missing_mask)[0])[0]
+    observed_indices = torch.where((~missing_mask)[0])[0] ## WARNING: It works only if the missing parts in the batch are all the same!
     
     if condition_init is None: conditions = torch.tensor(conditioner.transform({k: v for k, v in condition_set.items()})).float().to(device)
     else: conditions = condition_init
 
-    if z_init is None: z = model.encoder.sample(model.encoder(torch.cat((x, conditions), dim=1)))[0]
+    if z_init is None: z = model.encoder.sample(model.encoder(torch.cat((x, conditions), dim=-1)))
     else: z = z_init
 
     for itx in range(num_iter):
@@ -75,13 +75,13 @@ def metropolis_within_gibbs(model, x, missing_mask, conditioner, condition_set, 
             num_accepted_in_epoch = 0
             print(f"Iteration: {itx}")
 
-        z_params = model.encoder(torch.cat((x, conditions), dim=1))
+        z_params = model.encoder(torch.cat((x, conditions), dim=-1))
 
-        x_params = model.decoder(torch.cat((z, conditions),dim=1))
+        x_params = model.decoder(torch.cat((z, conditions), dim=-1))
 
-        z_proposal = model.encoder.sample(model.encoder(torch.cat((x, conditions), dim=1)))[0]
+        z_proposal = model.encoder.sample(model.encoder(torch.cat((x, conditions), dim=-1)))[0]
 
-        x_params_proposal = model.decoder(torch.cat((z_proposal, conditions),dim=1))
+        x_params_proposal = model.decoder(torch.cat((z_proposal, conditions), dim=-1))
 
         loglikelihood_proposal = model.decoder.log_likelihood(x, param_dict=x_params_proposal) + model.encoder.log_likelihood(z_proposal, param_dict=model.prior_params) - model.encoder.log_likelihood(z_proposal, param_dict=z_params)
 
@@ -91,20 +91,21 @@ def metropolis_within_gibbs(model, x, missing_mask, conditioner, condition_set, 
 
         rho = torch.minimum(torch.tensor([1]), torch.exp(log_ratio.cpu()))
 
-        eps = torch.rand((1,))
+        eps = torch.rand((rho.shape[0],))
+        accept_idx = torch.where(eps<rho)[0]
 
-        if eps<rho:
-            z = z_proposal*1.0
-            x_params = x_params_proposal.copy()
-            num_accepted_in_epoch += 1
+
+        z[accept_idx] = z_proposal[accept_idx]*1.0
+        for k,v in x_params_proposal.items(): x_params[k][accept_idx] = v[accept_idx]*1.0
+        num_accepted_in_epoch += accept_idx.shape[0]
 
         cond_sample, _, _ = sample_conditional(model.decoder, x_params, x[:,observed_indices], observed_indices)
-            
-        x[missing_mask] = cond_sample*1.0
+        
+        x[missing_mask] = cond_sample.flatten()
 
-        x_samples.append(x[0].cpu().tolist())
-        z_samples.append(z[0].cpu().tolist())
-        conditions_list.append(conditions[0].cpu().tolist())
+        x_samples.append(x.cpu().tolist())
+        z_samples.append(z.cpu().tolist())
+        conditions_list.append(conditions.cpu().tolist())
 
         if itx >= num_samples: 
             x_samples.pop(0)
@@ -113,7 +114,7 @@ def metropolis_within_gibbs(model, x, missing_mask, conditioner, condition_set, 
 
         conditions = torch.tensor(conditioner.transform({k: v for k, v in condition_set.items()})).float().to(device)
 
-        if (itx+1)%verbose_freq==0: print(f"Acceptance ratio: %{num_accepted_in_epoch/verbose_freq*100}")
+        if (itx+1)%verbose_freq==0: print(f"Acceptance ratio: %{num_accepted_in_epoch/(verbose_freq*x.shape[0])*100}")
     return np.array(x_samples), np.array(z_samples), np.array(conditions_list)
 
 @torch.no_grad()
@@ -134,12 +135,12 @@ def cvae_imputation(model, data, conditioner, condition_set, num_samples=10, num
 
     if num_iter > 0:
         print("Metropolis-within-Gibbs is starting...")
-        x_samples, z_samples, condition_samples = metropolis_within_gibbs(model, torch.tensor(x_samples[0])[None,...].float().to(device), 
+        x_samples, z_samples, condition_samples = metropolis_within_gibbs(model, torch.tensor(x_samples[0]).float().to(device), 
                                                                           missing_mask, 
                                                                           conditioner, 
                                                                           condition_set, 
-                                                                          z_init=torch.tensor(z_samples[0])[None,...].float().to(device), 
-                                                                          condition_init=torch.tensor(condition_samples[0])[None,...].float().to(device), 
+                                                                          z_init=torch.tensor(z_samples[0]).float().to(device), 
+                                                                          condition_init=torch.tensor(condition_samples[0]).float().to(device), 
                                                                           num_samples=num_samples, 
                                                                           num_iter=num_iter, 
                                                                           verbose_freq=verbose_freq, 
@@ -150,4 +151,5 @@ def cvae_imputation(model, data, conditioner, condition_set, num_samples=10, num
     model.prior_params = {key: value.to("cpu") for key, value in model.prior_params.items()}
 
     return x_samples, z_samples, condition_samples
+
 
