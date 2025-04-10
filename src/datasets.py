4c49477c8eb59b63efa4e7417197e518b83aa5bd
kabolat <k.bolat@tudelft.nl>
2025-04-10 15:36:34 +0200

    Add num_samples parameter to ConditionedDataset for flexible conditioning

--- src/datasets.py
+++ src/datasets.py
@@ -8,17 +8,18 @@ from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
 from .utils import *
 
 class ConditionedDataset(torch.utils.data.Dataset):
-    def __init__(self, inputs, conditions=None, conditioner=None):
+    def __init__(self, inputs, conditions=None, conditioner=None, num_samples=1):
         self.inputs = inputs
         self.conditions = conditions ##raw
         self.conditioner = conditioner
+        self.num_samples = num_samples
 
     def __len__(self):
         return self.inputs.shape[0]
     
     def __getitem__(self, idx):
         input_ = self.inputs[idx]
-        condition_ = self.conditioner.transform({key: condition[[idx]] for key, condition in self.conditions.items()}).squeeze()
+        condition_ = self.conditioner.transform({key: condition[[idx]] for key, condition in self.conditions.items()}, num_samples=self.num_samples).squeeze()
         return torch.tensor(input_).float(), torch.tensor(condition_).float()
     
 class ContexedDataset(torch.utils.data.Dataset):
